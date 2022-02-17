32bbcf4ea51e1489da339b59413b9b8b035ec745
erginbostanci <erginbostanci@yahoo.com>
2022-02-17 03:45:45 -0600

    Monday Total Recall

--- src/main/java/com/revature/pokedex/SearchFormService.java
+++ src/main/java/com/revature/pokedex/SearchFormService.java
@@ -16,12 +16,14 @@ public class SearchFormService extends HttpServlet {
                 "Search Pokedex</Title>\n" +
                 "</Head>\n" +
                 "<Body>\n" +
+                "    <h1>Pokedex Search</h1>" +
                 "    <form action='/pokemon'>\n" +
                 "    " +
                 "    " +
                 "<input type='name' name='searchName'/>" +
                 "        \n" +
                 "        <input type='submit' value='Search'/>\n" +
+                "    <a href ='/pokemon'>See full Pokedex </a>\n" +
                 "    </form>\n" +
                 "</Body>\n" +
                 "</Html>";
